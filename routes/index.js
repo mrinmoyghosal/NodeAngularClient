c51f234e3f6575708b0c96bca2185d8b78141e67
mrinmoyghosal <techkiddy@gmail.com>
2018-05-07 10:28:15 +0530

    Minor Changes on Variables

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,29 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
+
+var options={
+    uri:'/oauth/token',
+    baseUrl:'http://localhost:8889',
+    method:'POST',
+    json:true,
+    body:{"client_id":"admin","client_secret":"getroot","grant_type":"client_credentials","scope":"restaurent"},
+  }
+
+  var options_2={
+    uri:'/restaurent',
+    baseUrl:'http://localhost:8889',
+    method:'GET'
+  }
+
+  var bearerOptions={
+        uri:'/restaurent',
+        baseUrl:'http://localhost:8889',
+        auth:{
+          bearer:''
+        }
+      }
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Angular + Node - OAuth Client Credential Grant Example ' });
@@ -9,19 +32,7 @@ router.get('/', function(req, res) {
 
 router.get('/restaurent', function(req, res) {
 
-  var options={
-  	uri:'/oauth/token',
-  	baseUrl:'http://localhost:8889',
-  	method:'POST',
-  	json:true,
-  	body:{"client_id":"admin","client_secret":"getroot","grant_type":"client_credentials","scope":"restaurent"},
-  }
-
-  var options_2={
-  	uri:'/restaurent',
-  	baseUrl:'http://localhost:8889',
-  	method:'GET'
-  }
+  
 
   request(options_2,function(e,r,b){
   	res.setHeader('Content-Type', 'application/json');
@@ -38,25 +49,13 @@ router.get('/restaurent', function(req, res) {
 
 router.get('/restaurentAdmin', function(req, res) {
 
-  var options={
-  	uri:'/oauth/token',
-  	baseUrl:'http://localhost:8889',
-  	method:'POST',
-  	json:true,
-  	body:{"client_id":"admin","client_secret":"getroot","grant_type":"client_credentials","scope":"restaurent"},
-  }
+  
 
   
 
    request(options,function(e,r,b){
    	
-   		var bearerOptions={
-   			uri:'/restaurent',
-   			baseUrl:'http://localhost:8889',
-   			auth:{
-   				bearer:b.access_token
-   			}
-   		}
+   		bearerOptions['auth']['bearer']=b.access_token;
 
    		request(bearerOptions,function(e,r,b){
    			res.setHeader('Content-Type', 'application/json');
@@ -72,26 +71,10 @@ router.get('/restaurentUpdate', function(req, res) {
 
  console.log('I am called');
 
-  var options={
-  	uri:'/oauth/token',
-  	baseUrl:'http://localhost:8889',
-  	method:'POST',
-  	json:true,
-  	body:{"client_id":"admin","client_secret":"getroot","grant_type":"client_credentials","scope":"restaurent"},
-  }
-
-  
 
    request(options,function(e,r,b){
    	
-   		var bearerOptions={
-   			uri:'/restaurentUpdate',
-   			baseUrl:'http://localhost:8889',
-   			qs:{id:req.query.id,Name:req.query.Name,Address:req.query.Address,open:req.query.open,close:req.query.close},
-   			auth:{
-   				bearer:b.access_token
-   			}
-   		}
+   		bearerOptions['auth']['bearer']=b.access_token;
 
    		request(bearerOptions,function(e,r,b){
    			res.setHeader('Content-Type', 'application/json');
